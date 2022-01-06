db2849be4b9db418880625a807515b0f76218433
shaywacker <shay2442@yahoo.com>
2022-01-06 17:16:04 -0600

    commit tree fecbe79aaedfafdc558c3820404c4eba52aecebc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,39 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  get '/games' do
-    games = Game.all.order(:title).limit(10)
-    games.to_json
-  end
-
   get '/games/:id' do
     game = Game.find(params[:id])
 
-    game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+    # include associated reviews in the JSON response
+    game.to_json(include: :reviews)
   end
+
+    delete '/reviews/:id' do
+   
+      review = Review.find(params[:id])
+      review.destroy
+      review.to_json
+    end
+
+    post '/reviews' do
+      post '/reviews' do
+        review = Review.create(
+          score: params[:score],
+          comment: params[:comment],
+          game_id: params[:game_id],
+          user_id: params[:user_id]
+        )
+        review.to_json
+      end
+
+      patch '/reviews/:id' do
+        review = Review.find(params[:id])
+        review.update(
+          score: params[:score],
+          comment: params[:comment]
+        )
+        review.to_json
+      end
+    end
 
 end
